6780bf9955635c1180778d5edbdccb9c4e7c4bd0
Ant <ant.griffiths22@gmail.com>
2023-05-08 23:05:17 +0100

    Complete events challenge

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
-$(document).ready(function() {
-    $("#stream1_btn").on("click", function() {
-        $(".stream1").removeClass('highlight_stream');
-        $(".stream2").removeClass('highlight_stream');
-        $(".stream3").removeClass('highlight_stream');
-        $(".stream1").addClass('highlight_stream');
-   });
-   $("#stream2_btn").on("click", function() {
-        $(".stream1").removeClass('highlight_stream');
-        $(".stream2").removeClass('highlight_stream');
-        $(".stream3").removeClass('highlight_stream');
-        $(".stream2").addClass('highlight_stream');
-   });
-   $("#stream3_btn").on("click", function() {
-        $(".stream1").removeClass('highlight_stream');
-        $(".stream2").removeClass('highlight_stream');
-        $(".stream3").removeClass('highlight_stream');
-        $(".stream3").addClass('highlight_stream');
-   });
-}); 
+$(document).ready(function () {
+    $("#stream1_btn").on("click", function () {
+        $(".stream1").removeClass("highlight_stream");
+        $(".stream2").removeClass("highlight_stream");
+        $(".stream3").removeClass("highlight_stream");
+        $(".stream1").addClass("highlight_stream");
+    });
+    $("#stream2_btn").on("click", function () {
+        $(".stream1").removeClass("highlight_stream");
+        $(".stream2").removeClass("highlight_stream");
+        $(".stream3").removeClass("highlight_stream");
+        $(".stream2").addClass("highlight_stream");
+    });
+    $("#stream3_btn").on("click", function () {
+        $(".stream1").removeClass("highlight_stream");
+        $(".stream2").removeClass("highlight_stream");
+        $(".stream3").removeClass("highlight_stream");
+        $(".stream3").addClass("highlight_stream");
+    });
+    $("p").on("click", function () {
+        $("p").css("color", "red");
+    });
+    $("h2").on("mouseenter", function () {
+        $("h2").addClass("blue-background");
+    });
+    $("h2").on("mouseenter", function (event) {
+        $(event.target).addClass("text-grow");
+    });
+    $(".bottom_button").on("mouseenter", function() {
+        $("body").removeClass("grey-background").addClass("black-background");
+    });
+    $(".bottom_button").on("mouseleave", function() {
+        $("body").removeClass("black-background").addClass("grey-background");
+    });
+
+});
